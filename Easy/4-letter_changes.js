f66fb25d4d81eb9b4c2b6f3dcec042ee9ee9aa39
markstewart <markstewy@gmail.com>
2016-06-20 19:08:43 -0600

    4-letter_changes was a little messy, but I cleaned it up by adding upper and lowercase vowels to the alphabet key array

--- Easy/4-letter_changes.js
+++ Easy/4-letter_changes.js
@@ -6,3 +6,26 @@
 //
 // Input:"fun times!"
 // Output:"gvO Ujnft!"
+
+
+var change = function(str) {
+        var alphabet = ["a", "b", "c", "d", "E", "e", "f", "g", "h", "I", "i", "j", "k", "l", "m", "n", "O", "o", "p", "q", "r", "s", "t", "U", "u", "v", "w", "x", "y", "z"];
+        //**the alphabet array key includes two of each vowel so I don't have to write any if statemnt to change vowels back to upper case.**
+        var changedArr = [];
+        var strArr = str.split("")
+
+        for (var i = 0; i < strArr.length; i++) {
+            //console.log(alphabet[alphabet.indexOf(strArr[i].toLowerCase())]);
+            if ((strArr[i].toLowerCase()) === "z") {
+                changedArr.push("A");
+            } else if (alphabet.indexOf(strArr[i].toLowerCase()) !== -1) {
+                changedArr.push(alphabet[alphabet.indexOf(strArr[i].toLowerCase()) + 1]);
+            } else {
+                changedArr.push(strArr[i]);
+            }
+        } //end for loop
+        return changedArr.join("");
+    } //end function
+
+change("zadHCn t123a!@#")
+    //change("fun times!")
---
+++ Easy/5-simple_adding.js
@@ -0,0 +1,8 @@
+// Challenge
+// Using the JavaScript language, have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000.
+// Sample Test Cases
+// Input:12
+// Output:78
+//
+// Input:140
+// Output:9870
