90165a96d2b9da786158d5200f8d70208a9fc75f
markstewart <markstewy@gmail.com>
2016-06-30 01:10:59 -0600

    added 24-27 easy

--- Easy/28-additive_persistence.js
+++ Easy/28-additive_persistence.js
@@ -1,5 +1,8 @@
 // Challenge
-// Using the JavaScript language, have the function AdditivePersistence(num) take the num parameter being passed which will always be a positive integer and return its additive persistence which is the number of times you must add the digits in num until you reach a single digit. For example: if num is 2718 then your program should return 2 because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9.
+// Using the JavaScript language, have the function AdditivePersistence(num) take the num parameter being passed which
+//will always be a positive integer and return its additive persistence which is the number of times you must add the
+//digits in num until you reach a single digit. For example: if num is 2718 then your program should return 2
+//because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9.
 // Sample Test Cases
 // Input:4
 // Output:0
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Am I the only one that thinks toy problems are addicting?
 
-Here are my solutions to the coderbyte javascript challenges. No... I didn't peek at solutions (unless noted in all caps in my comments) just the occasional googling of a method or regex.
+Here are my solutions to the coderbyte javascript challenges. No... I didn't peek at any solutions (unless noted in all caps in my toy problem comments) just the occasional googling of a javascript method or regex.
 
 https://coderbyte.com/challenges/#medChals
