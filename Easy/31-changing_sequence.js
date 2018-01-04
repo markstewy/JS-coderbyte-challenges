bacddbda8b07929cb2047c8b6433edaa2db6e698
mark stewart <mstewart@inmoment.com>
2018-01-04 12:45:38 -0700

    forgot to save

--- Easy/31-changing_sequence.js
+++ Easy/31-changing_sequence.js
@@ -13,27 +13,24 @@
 // Output:3
 
 
+function dirIncrease (arr) {
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false
+        } else if (arr[i] > arr[i - 1]) {
+            return true
+        }
+    }
+}
 
 function ChangingSequence(arr) {
-    let op;
-    //find the first change and identify increase or decrease
-    for(var i = 1; i < arr.length; i++) {
-        if(arr[i] > arr[i - 1]) {
-          if(!op) {
-            op = 'greater'
-          } else if(op !== 'greater') {
+    let increase = dirIncrease(arr);
+    for (var i = 1; i < arr.length; i++) {
+        if ((arr[i] >= arr[i - 1]) !== increase)  {
+            //^^^toggle >= and > depending on if you consider an equal number to reflect a change in direction or not
             return i - 1
-          }
-        } else if(arr[i] < arr[i - 1]) {
-          if(!op) {
-            op = 'less'
-          } else if(op !== 'less') {
-            return i - 1
-          }
         }
     }
 }
 
 ChangingSequence([1, 2, 4, 6, 4, 3, 1]);
-
-//this solution is meant to handle any number of equal values at the beginning or in the middle of the array without breaking
