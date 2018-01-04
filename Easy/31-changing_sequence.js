8e425933b16311fe3ff4c3c7e6ebdd7939000843
mark stewart <mstewart@inmoment.com>
2018-01-04 12:45:01 -0700

    completed 31 changing sequence, opted for a callback function over excessively nested if statments

--- Easy/31-changing_sequence.js
+++ Easy/31-changing_sequence.js
@@ -1,8 +1,39 @@
 // Challenge
-// Using the JavaScript language, have the function ChangingSequence(arr) take the array of numbers stored in arr and return the index at which the numbers stop increasing and begin decreasing or stop decreasing and begin increasing. For example: if arr is [1, 2, 4, 6, 4, 3, 1] then your program should return 3 because 6 is the last point in the array where the numbers were increasing and the next number begins a decreasing sequence. The array will contain at least 3 numbers and it may contains only a single sequence, increasing or decreasing. If there is only a single sequence in the array, then your program should return -1. Indexing should begin with 0.
+// Using the JavaScript language, have the function ChangingSequence(arr) take the array of numbers stored 
+//in arr and return the index at which the numbers stop increasing and begin decreasing or stop decreasing 
+//and begin increasing. For example: if arr is [1, 2, 4, 6, 4, 3, 1] then your program should return 3 because 6 is 
+//the last point in the array where the numbers were increasing and the next number begins a decreasing sequence. 
+//The array will contain at least 3 numbers and it may contains only a single sequence, increasing or decreasing. 
+//If there is only a single sequence in the array, then your program should return -1. Indexing should begin with 0.
 // Sample Test Cases
 // Input:-4, -2, 9, 10
 // Output:-1
 //
 // Input:5, 4, 3, 2, 10, 11
 // Output:3
+
+
+
+function ChangingSequence(arr) {
+    let op;
+    //find the first change and identify increase or decrease
+    for(var i = 1; i < arr.length; i++) {
+        if(arr[i] > arr[i - 1]) {
+          if(!op) {
+            op = 'greater'
+          } else if(op !== 'greater') {
+            return i - 1
+          }
+        } else if(arr[i] < arr[i - 1]) {
+          if(!op) {
+            op = 'less'
+          } else if(op !== 'less') {
+            return i - 1
+          }
+        }
+    }
+}
+
+ChangingSequence([1, 2, 4, 6, 4, 3, 1]);
+
+//this solution is meant to handle any number of equal values at the beginning or in the middle of the array without breaking
