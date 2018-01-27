91cdbbb8ef7f1d3ffd1d5e9c295b6a17a0a32fd0
markstewart <markstewy@gmail.com>
2018-01-27 11:39:17 -0700

    removed space

--- Medium/40-binary_tree_lca.js
+++ Medium/40-binary_tree_lca.js
@@ -1,7 +1,6 @@
 // Challenge
 // Using the JavaScript language, have the function BinaryTreeLCA(strArr) take the array of strings stored in strArr, which will contain 3 elements: the first element will be a binary tree with all unique values in a format similar to how a binary heap is implemented with NULL nodes at any level represented with a #, the second and third elements will be two different values, and your goal is to find the lowest common ancestor of these two values. For example: if strArr is
 // ["[12, 5, 9, 6, 2, 0, 8, #, #, 7, 4, #, #, #, #]", "6", "4"] then this tree looks like the following:
-
 //
 //
 //
